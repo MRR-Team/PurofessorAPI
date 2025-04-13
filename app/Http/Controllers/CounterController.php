2cf8e990fe4daa5e1ebec6848c266593f8815a2c
Zielinski-Marcel <marcel.zielinski@studenci.collegiumwitelona.pl>
2025-04-13 18:17:23 +0200

    Champion Counter

---
+++ app/Http/Controllers/CounterController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Actions\BestCounterAction;
+use App\Models\Champion;
+use Illuminate\Http\JsonResponse;
+
+class CounterController extends Controller
+{
+   public function __invoke(BestCounterAction $bestCounterAction, String $role, Champion $enemyChampion):JsonResponse{
+       return response()->json($bestCounterAction((array)Champion::all(),$role, $enemyChampion));
+   }
+}
